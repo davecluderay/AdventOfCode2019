043f2194560d4b73c8033f32d918b012ea80c9eb
Dave Cluderay <dave.cluderay@bybox.com>
2019-11-30 17:30:50 +0000

    Add project for day 1.

---
+++ Aoc2019-Day01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aoc2019_Day01
+{
+    public static class Program
+    {
+        private static void Run()
+        {
+            var lines = InputFile.ReadAllLines();
+        }
+
+        public static void Main()
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception exception)
+            {
+                Console.Error.WriteLine(exception);
+            }
+            finally
+            {
+                Console.WriteLine("Done.");
+            }
+        }
+    }
+}
