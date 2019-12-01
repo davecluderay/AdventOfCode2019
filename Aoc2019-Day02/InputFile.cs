16f448f6530da5f524411e124251781d160c6c60
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-01 19:20:22 +0000

    Add project for day 2.

---
+++ Aoc2019-Day02/InputFile.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Aoc2019_Day02
+{
+    internal static class InputFile
+    {
+        public static string[] ReadAllLines(string fileName = null)
+        {
+            var directoryPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            var filePath      = Path.Combine(directoryPath, fileName ?? "input.txt");
+            if (!File.Exists(filePath)) throw new Exception($"File not found: {filePath}");
+
+            var lines = File.ReadAllLines(filePath);
+            Console.WriteLine($"Read {lines.Length} line(s) from input file: {filePath}");
+            
+            return lines;
+        }
+    }
+}
---
+++ Aoc2019-Day02/OutputFile.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace Aoc2019_Day02
+{
+    internal static class OutputFile
+    {
+        public static void WriteAllLines(IEnumerable<string> lines, string fileName = null)
+        {
+            var directoryPath = Path.GetDirectoryName(Assembly.GetCallingAssembly().Location);
+            var filePath      = Path.Combine(directoryPath, fileName ?? "output.txt");
+
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
