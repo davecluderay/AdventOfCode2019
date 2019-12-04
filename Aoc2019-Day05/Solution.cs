5f309d5d70295de792f721791172a4cf65cb2653
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-04 08:47:18 +0000

    Add project for day 5.

---
+++ Aoc2019-Day05/Solution.cs
@@ -0,0 +1,19 @@
+namespace Aoc2019_Day05
+{
+    internal class Solution
+    {
+        public string Title => "Day 5";
+
+        public object PartOne()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+    }
+}
