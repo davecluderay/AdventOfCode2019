c252108a29a6c7e112ea1fed1b260a67e32b7a4f
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-06 06:06:47 +0000

    Day 6 solution.

--- Aoc2019-Day06/Solution.cs
+++ Aoc2019-Day06/Solution.cs
@@ -1,19 +1,58 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Aoc2019_Day06
 {
     internal class Solution
     {
-        public string Title => "";
+        public string Title => "Day 6: Universal Orbit Map";
 
         public object PartOne()
         {
-            var lines = InputFile.ReadAllLines();
-            return null;
+            var data = InputFile.ReadAllLines()
+                                .Select(l => l.Split(')'))
+                                .Select(p => (body: p[0], satellite: p[1]));
+            var directOrbits = data.ToDictionary(o => o.satellite, o => o.body);
+            
+            var checksum = directOrbits.Count;
+            foreach (var key in directOrbits.Keys)
+            {
+                var next = directOrbits[key];
+                while (directOrbits.ContainsKey(next))
+                {
+                    next = directOrbits[next];
+                    checksum++;
+                }
+            }
+            
+            return checksum;
         }
         
         public object PartTwo()
         {
-            var lines = InputFile.ReadAllLines();
-            return null;
+            var data = InputFile.ReadAllLines()
+                                .Select(l => l.Split(')'))
+                                .Select(p => (body: p[0], satellite: p[1]));
+            var directOrbits = data.ToDictionary(o => o.satellite, o => o.body);
+
+            IEnumerable<string> FindOrbitChain(string satellite)
+            {
+                var next = directOrbits[satellite];
+                yield return next;
+                while (directOrbits.ContainsKey(next))
+                {
+                    yield return next = directOrbits[next];
+                }
+            }
+            
+            var youOrbitChain = FindOrbitChain("YOU").ToArray();
+            var sanOrbitChain = FindOrbitChain("SAN").ToArray();
+
+            var firstCommonBody = youOrbitChain.First(p => sanOrbitChain.Contains(p));
+            var youToCommonBody = youOrbitChain.TakeWhile(p => p != firstCommonBody).Count();
+            var sanToCommonBody = sanOrbitChain.TakeWhile(p => p != firstCommonBody).Count();
+            
+            return youToCommonBody + sanToCommonBody;
         }
     }
 }
