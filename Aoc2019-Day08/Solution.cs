6fd715055cbca961b06399dc3148b7d59638e3dc
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-07 23:25:44 +0000

    Add project for day 8.

---
+++ Aoc2019-Day08/Solution.cs
@@ -0,0 +1,17 @@
+namespace Aoc2019_Day08
+{
+    internal class Solution
+    {
+        public string Title => "Day 8";
+
+        public object PartOne()
+        {
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            return null;
+        }
+    }
+}
