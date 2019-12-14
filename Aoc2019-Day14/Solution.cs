15eb23a8bd6bc7c02b0f181243537473efb7f0c2
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-14 02:14:15 +0000

    Add project for day 14.
    Add project for day 14.
    
    Add project for day 14.

---
+++ Aoc2019-Day14/Solution.cs
@@ -0,0 +1,17 @@
+namespace Aoc2019_Day14
+{
+    internal class Solution
+    {
+        public string Title => "Day 14";
+
+        public object PartOne()
+        {
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            return null;
+        }
+    }
+}
