e88edaee53867848db556b1802ee02810d1ce75a
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-24 20:41:07 +0000

    Add project for day 20.

---
+++ Aoc2019-Day20/Solution.cs
@@ -0,0 +1,19 @@
+namespace Aoc2019_Day20
+{
+    internal class Solution
+    {
+        public string Title => "Day 20: Donut Maze";
+
+        public object PartOne()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            var lines = InputFile.ReadAllLines();
+            return null;
+        }
+    }
+}
