eaef98e0ef6599ddc2f7a9856156882edd712363
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-30 00:28:20 +0000

    Add project for day 22.

---
+++ Aoc2019-Day22/Solution.cs
@@ -0,0 +1,17 @@
+namespace Aoc2019_Day22
+{
+    internal class Solution
+    {
+        public string Title => "Day 22: Slam Shuffle";
+
+        public object PartOne()
+        {
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            return null;
+        }
+    }
+}
