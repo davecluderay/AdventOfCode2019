17c3c9d24cd464f8c9709896c50f9e11e6db90ad
Dave Cluderay <dave.cluderay@bybox.com>
2019-12-30 00:25:59 +0000

    Day 21 solution.

---
+++ Aoc2019-Day21/Computer/IntCodeComputer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aoc2019_Day21.Computer
+{
+    internal class IntCodeComputer
+    {
+        private readonly DebugOutput _debug;
+
+        private readonly IntCodeMemory _memory = new IntCodeMemory();
+
+        public IntCodeComputer(DebugOutput debug = null)
+        {
+            _debug = debug ?? new DebugOutput();
+        }
+
+        public void LoadProgram(string fileName = null)
+        {
+            var program = InputFile.ReadAllText(fileName)
+                                   .Split(',')
+                                   .Select(long.Parse)
+                                   .ToArray();
+            _memory.Set(0, program);
+        }
+
+        public IEnumerable<long> RunProgram()
+        {
+            return RunProgram(() => throw new InvalidOperationException("No input available."));
+        }
+        
+        public IEnumerable<long> RunProgram(Func<long> inputGenerator)
+        {
+            var instructionPointer = 0L;
+            var relativeBase = 0L;
+            var running = true;
+            while (running)
+            {
+                var opcode = (Opcode)(_memory.GetAt(instructionPointer) % 100);
+                switch (opcode)
+                {
+                    case Opcode.Add:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 3);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 3);
+                        var operand1 = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var operand2 = parameters[1].DereferencedValue(_memory, relativeBase);
+                        var address = parameters[2].Value(relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        SetMemory(address, operand1 + operand2);
+                        
+                        instructionPointer += 4;
+                        break;
+                    }
+                    case Opcode.Multiply:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 3);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 3);
+                        var operand1 = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var operand2 = parameters[1].DereferencedValue(_memory, relativeBase);
+                        var address = parameters[2].Value(relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+
+                        SetMemory(address, operand1 * operand2);
+                        instructionPointer += 4;
+                        break;
+                    }
+                    case Opcode.Input:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 1);
+
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 1);
+                        var input = inputGenerator.Invoke();
+                        var address = parameters[0].Value(relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        SetMemory(address, input);
+                        instructionPointer += 2;
+                        break;
+                    }
+                    case Opcode.Output:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 1);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 1);
+                        var operand = parameters[0].DereferencedValue(_memory, relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+
+                        yield return operand;
+                        instructionPointer += 2;
+                        break;
+                    }
+                    case Opcode.JumpIfTrue:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 2);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 2);
+                        var operand = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var address = parameters[1].DereferencedValue(_memory, relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        instructionPointer = (operand != 0) ? address : (instructionPointer + 3);
+                        break;
+                    }
+                    case Opcode.JumpIfFalse:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 2);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 2);
+                        var operand    = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var address    = parameters[1].DereferencedValue(_memory, relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        instructionPointer = (operand == 0) ? address : (instructionPointer + 3);
+                        break;
+                    }
+                    case Opcode.LessThan:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 3);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 3);
+                        var operand1 = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var operand2 = parameters[1].DereferencedValue(_memory, relativeBase);
+                        var address = parameters[2].Value(relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        SetMemory(address, (operand1 < operand2) ? 1 : 0);
+                        instructionPointer += 4;
+                        break;
+                    }
+                    case Opcode.Equals:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 3);
+                        
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 3);
+                        var operand1   = parameters[0].DereferencedValue(_memory, relativeBase);
+                        var operand2   = parameters[1].DereferencedValue(_memory, relativeBase);
+                        var address    = parameters[2].Value(relativeBase);
+                        
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+                        
+                        SetMemory(address, (operand1 == operand2) ? 1 : 0);
+                        instructionPointer += 4;
+                        break;
+                    }
+                    case Opcode.RelativeBaseOffset:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 1);
+                        var parameters = OpcodeParameter.ReadParameters(_memory, instructionPointer, 1);
+                        var operand    = parameters[0].DereferencedValue(_memory, relativeBase);
+
+                        _debug.WriteInstruction(_memory, relativeBase, opcode, parameters);
+
+                        relativeBase += operand;
+                        instructionPointer += 2;
+                        break;
+                    }
+                    case Opcode.Halt:
+                    {
+                        _debug.WriteRawInstruction(_memory, instructionPointer, 0);
+                        _debug.WriteInstruction(_memory, relativeBase, opcode);
+                        
+                        running = false;
+                        instructionPointer += 1;
+                        break;
+                    }
+                    default:
+                        throw new Exception($"Unrecognised opcode: {opcode}");
+                }
+            }
+        }
+
+        public void SetMemory(long position, long value)
+        {
+            _debug.Write($"Writing {value} to position {position}");
+            _memory.SetAt(position, value);
+        }
+    }
+}
--- Aoc2019-Day21/Solution.cs
+++ Aoc2019-Day21/Solution.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aoc2019_Day21.Computer;
+
 namespace Aoc2019_Day21
 {
     internal class Solution
@@ -6,12 +12,67 @@ namespace Aoc2019_Day21
 
         public object PartOne()
         {
-            return null;
+            var computer = new IntCodeComputer();
+            computer.LoadProgram();
+
+            var inputs = GenerateInputs("NOT J T", // SET T=1
+                                        "OR D J",  // SET J=1 IF LANDING POSITION (D) HAS FLOOR
+                                        "AND A T", // WORK OUT IF PRECEDING POSITIONS ARE ALL FLOOR
+                                        "AND B T",
+                                        "AND C T",
+                                        "NOT T T", // SET T=1 ONLY IF PRECEDING POSITIONS ARE NOT ALL FLOOR
+                                        "AND T J", // SET J=1 ONLY IF IT WAS ALREADY SET AND THE PRECEDING POSITIONS ARE NOT ALL FLOOR
+                                        "WALK");
+            long result = 0;
+            foreach (var output in computer.RunProgram(inputs))
+            {
+                if (output < 0 || output > byte.MaxValue)
+                    result = output;
+                else
+                    Console.Write((char)output);
+            }
+
+            return result;
         }
         
         public object PartTwo()
         {
-            return null;
+            var computer = new IntCodeComputer();
+            computer.LoadProgram();
+
+            var inputs = GenerateInputs("NOT J T", // SET T=1
+                                        "OR D J",  // SET J=1 IF LANDING POSITION (D) HAS FLOOR
+                                        "AND A T", // WORK OUT IF PRECEDING POSITIONS ARE ALL FLOOR
+                                        "AND B T",
+                                        "AND C T",
+                                        "NOT T T", // SET T=1 ONLY IF PRECEDING POSITIONS ARE NOT ALL FLOOR
+                                        "AND T J", // SET J=1 ONLY IF IT WAS ALREADY SET AND THE PRECEDING POSITIONS ARE NOT ALL FLOOR
+                                        "AND H T", // SET J=1 ONLY IF IT WAS ALREADY SET AND A SECOND JUMP OR STEP WOULD BE TO FLOOR
+                                        "OR E T",
+                                        "AND T J",
+                                        "RUN");
+            long result = 0;
+            foreach (var output in computer.RunProgram(inputs))
+            {
+                if (output < 0 || output > byte.MaxValue)
+                    result = output;
+                else
+                    Console.Write((char)output);
+            }
+
+            return result;
+        }
+            
+        private Func<long> GenerateInputs(params string[] inputs)
+        {
+            var data = new Queue<long>(Encoding.ASCII.GetBytes(string.Join("\n", inputs.Append("")))
+                                               .Select(@byte => (long)@byte));
+            return () =>
+                   {
+                       var input = data.Dequeue();
+                       Console.Write((char)input);
+                       return input;
+                   };
         }
     }
 }
