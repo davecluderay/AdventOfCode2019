45fcf692b090566512bc43f20effc453a18c1f63
Dave Cluderay <dave.cluderay@bybox.com>
2020-01-04 01:21:55 +0000

    Add project for day 24.

---
+++ Aoc2019-Day24/Solution.cs
@@ -0,0 +1,17 @@
+namespace Aoc2019_Day24
+{
+    internal class Solution
+    {
+        public string Title => "Day 24: Planet of Discord";
+
+        public object PartOne()
+        {
+            return null;
+        }
+        
+        public object PartTwo()
+        {
+            return null;
+        }
+    }
+}
